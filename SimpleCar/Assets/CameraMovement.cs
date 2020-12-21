1baae26b8d20b105ad587b9749e89478ceffd3be
Kagan Ayten <kaan.aytn@hotmail.com>
2020-12-22 00:29:22 +0300

    SimpleCarController

---
+++ SimpleCar/Assets/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(WaitForStart());
+    }
+    IEnumerator WaitForStart()
+    {
+        yield return new WaitForSeconds(2);
+        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * 2);
+    }
+}
