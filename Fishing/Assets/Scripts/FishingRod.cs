a32c34fc69dde814c9a2d5e6615dc55da2515319
Kagan Ayten <kaan.aytn@hotmail.com>
2021-01-21 17:28:23 +0300

    FishingSystem

---
+++ Fishing/Assets/Scripts/FishingRod.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FishingRod : MonoBehaviour
+{
+    public GameObject fishHook;
+    public LineRenderer rope;
+    public Transform startPos;
+    private Rigidbody hookRB;
+
+    public GameObject warning;
+
+    public Text fishCountText;
+
+    public bool isSent;
+    public bool isTakedBack;
+
+    public bool isCatched;
+
+    public int fishCount;
+    float number =0;
+
+    [SerializeField]
+    [Range(1, 50)]
+    int throwSpeed;
+    void Start()
+    {
+        hookRB = fishHook.GetComponent<Rigidbody>();
+        
+    }
+
+    void Update()
+    {
+
+        fishCountText.text = "Your fish count is: " + fishCount.ToString();
+        rope.positionCount = 2;
+        rope.SetPosition(0, startPos.position);
+        rope.SetPosition(1, fishHook.transform.position);
+
+
+        if (isSent)
+        {
+            CatchFish();
+        }
+        if (Input.GetKeyDown("k")&& isSent !=true)
+        {
+            SendHook();
+        }
+        if (Input.GetKeyDown("m") && isSent == true && isTakedBack == false)
+        {
+            TakeHookBack();
+        }
+
+    }
+    void SendHook()
+    {
+        isTakedBack = false;
+        isSent = true;
+        hookRB.constraints = RigidbodyConstraints.None;
+        hookRB.AddForce(-Vector3.forward * throwSpeed, ForceMode.Impulse);
+        
+    }
+    void TakeHookBack()
+    {
+        isSent = false;
+        isTakedBack = true;
+        hookRB.constraints = RigidbodyConstraints.FreezeAll;
+        fishHook.transform.position = startPos.position;
+        if (isCatched == true)
+        {
+            fishCount++;
+            isCatched = false;
+           
+        }
+        warning.SetActive(false);
+        number = 0;
+    }
+    void CatchFish()
+    {
+        
+        number += Time.deltaTime;
+        if (number >= 5)
+        {
+            isCatched = true;
+            warning.SetActive(true);
+        }
+       if (number > 10)
+        {
+            number = 0;
+            warning.SetActive(false);
+            isCatched = false;
+        }
+
+    }
+}
---
+++ Fishing/Assets/Scripts/Rod.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rod : MonoBehaviour
+{
+    public GameObject fishHook;
+    public LineRenderer rope;
+    public Transform startPosition;
+
+    public GameObject warning;
+    public Text fishCountText;
+
+    [SerializeField]
+    [Range(1,50)]
+    private int throwSpeed;
+
+    private Rigidbody hookRB;
+
+    bool isSent;
+    bool isTakedBack;
+
+    bool isCatched;
+    int fishCount;
+
+    float timer = 0;
+    void Start()
+    {
+        hookRB = fishHook.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fishCountText.text = "Your fish count:" + fishCount.ToString();
+        if (isSent)
+        {
+            CatchFish();
+        }
+        if (Input.GetKeyDown("k") && isSent != true)
+        {
+            ThrowHook();
+        }
+        if (Input.GetKeyDown("m") && isSent == true && isTakedBack == false)
+        {
+            TakeHookBack();
+        }
+
+        rope.positionCount = 2;
+        rope.SetPosition(0, startPosition.position);
+        rope.SetPosition(1, fishHook.transform.position);
+    }
+    void ThrowHook()
+    {
+        isSent = true;
+        isTakedBack = false;
+        hookRB.constraints = RigidbodyConstraints.None;
+        hookRB.AddForce(-Vector3.forward*throwSpeed, ForceMode.Impulse);
+    }
+    void TakeHookBack()
+    {
+        isSent = false;
+        isTakedBack = true;
+        hookRB.constraints = RigidbodyConstraints.FreezeAll;
+        fishHook.transform.position = startPosition.position;
+        if (isCatched == true)
+        {
+            fishCount++;
+            isCatched = false;
+        }
+        warning.SetActive(false);
+        timer = 0;
+
+    }
+    void CatchFish()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 5)
+        {
+            isCatched = true;
+            warning.SetActive(true);
+        }
+        if (timer > 10)
+        {
+            timer = 0;
+            isCatched = false;
+            warning.SetActive(false);
+        }
+    }
+}
