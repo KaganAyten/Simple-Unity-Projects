425a4ac4e9f023f1a91dec81631e62141a439c3e
Kagan Ayten <kaan.aytn@hotmail.com>
2021-01-28 20:01:20 +0300

    StickyBomb

---
+++ StickyBomb/Assets/Scripts/Bomb.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    Vector3 target;
+    public ParticleSystem explodeParticle;
+    public void SetDestination(Vector3 target)
+    {
+        this.target = target;
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime * 5);
+        if (Input.GetKeyDown("g"))
+        {
+            StartCoroutine(MakeExplode());
+        }
+    }
+    IEnumerator MakeExplode()
+    {
+        explodeParticle.Play();
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+}
---
+++ StickyBomb/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bombObject;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(camRay, out hit, 1000))
+            {
+                GameObject temp = Instantiate(bombObject);
+                temp.GetComponent<Bomb>().SetDestination(hit.point);
+            }
+        }
+        
+    }
+}
